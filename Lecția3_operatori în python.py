92525fc54bd4be37a49dd377b3ab58bf14822516
Țurcanu Iulita <iulitaturcanuu@gmail.com>
2026-09-15 01:13:46 +0300

    Add files via upload

---
+++ Lecția3_operatori în python.py
@@ -0,0 +1,38 @@
+#exercițiul_1
+a=8
+b=13
+perimetru=2*a+2*b
+print(perimetru)
+aria=a*b
+print(aria)
+
+#exercițiul_2
+total=95 #minute
+ore_întregi=total//60
+print(ore_întregi)
+minute_rămase=total%60
+print(minute_rămase)
+
+#exercițiul_3
+n=18
+nr_par=(n%2==0)
+print(nr_par)
+
+#exercițiul_4
+expresia_1=2+6*8 #50
+expresia_1_2=(2+6)*8 #64
+print(expresia_1)
+print(expresia_1_2)
+expresia_2=(9-7)*2 #4
+expresia_2_2=9-7*2 #-5
+print(expresia_2)
+print(expresia_2_2)
+expresia_3=10*5-5 #45
+exprsia_3_2=10*(5-5) #0
+print(expresia_3)
+print(exprsia_3_2)
+
+#exercițiul_5
+nota=8
+puține_absențe=True
+este_admis=(nota>=5)and(puține_absențe)
