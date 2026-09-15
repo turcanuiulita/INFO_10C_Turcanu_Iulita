282789228a6cd1f92fc9b966b1e3f0775dd708ce
Țurcanu Iulita <iulitaturcanuu@gmail.com>
2026-09-15 10:19:07 +0300

    Add files via upload

---
+++ Funcția PRINT 15-09-26 (pr 1 2).py
@@ -0,0 +1,15 @@
+#problema 1
+print('Struguri', '100', 'kg', sep='   ', end='.\n')
+print('Mere', '10', 'tone', sep='   ', end='.\n')
+print('Cartofi', '250', 'kg', sep='   ', end='.\n')
+print('Varză', '1000', 'q', sep='   ', end='.\n')
+
+#problema 2
+a=6
+b=8
+print('Suma',a,'+',b,'=',a+b, end='\n')
+print('Produsul',a,'*',b,'=',a*b, end='\n')
+print('Diferența',a,'-',b,'=',a-b, end='\n')
+print('Câtul împărțirii',a,'//',b,'=',a//b, end='\n') #câtul împărțirii
+print('Restul împărțirii',a,'%',b,'=',a%b, end='\n') #restul împărțirii
+print('Ridicarea la putere',a,'**',b,'=',a**b, end='\n') #ridicarea la putere
---
+++ Funcția PRINT 15-09-26 (pr 3).py
@@ -0,0 +1,5 @@
+#problema 3
+a=5
+print('Volum', a**3, 'm³',sep='   ', end='.\n')
+print('Aria', 6*a**2, 'm²',sep='   ', end='.\n')
+print('Perimetru', 12*a, 'm',sep='   ', end='.\n')
