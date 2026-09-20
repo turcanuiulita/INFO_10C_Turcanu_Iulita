e8c0fb33b8110a758da33df5a1729f5cb9498ba7
Țurcanu Iulita <iulitaturcanuu@gmail.com>
2026-09-20 23:32:34 +0300

    Add files via upload

---
+++ Lecția3_problema 4.py
@@ -0,0 +1,12 @@
+#exercițiul_4
+n=6
+print(n, "m", "=", n*100, "cm")
+print(n, "kg", '=', n*1000000, 'mg')
+print(n,'ani', '=', n*12, 'luni')
+print(n, 'ani', '=', n*52, 'săptămâni')
+print(n, 'ani', '=', n*365, 'zile')
+
+#exercițiul_5
+print('x', '<', '10',)
+print('y', '<', '5')
+print(x and y //1)
