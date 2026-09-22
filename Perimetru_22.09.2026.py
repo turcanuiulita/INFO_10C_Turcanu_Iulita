4a9564a81f8fc854b8ef7166fa8c61544027d06a
Țurcanu Iulita <iulitaturcanuu@gmail.com>
2026-09-22 10:01:16 +0300

    Add files via upload

---
+++ Perimetru_22.09.2026.py
@@ -0,0 +1,6 @@
+lungime=float(input('Lungimea='))
+latime=float(input('Latimea='))
+aria=lungime*latime
+perimetru=2*(lungime+latime)
+print('Aria dreptunghiului este=',round(aria,4))
+print('Perimetrul dreptunghiului este=',round(perimetru,2))
